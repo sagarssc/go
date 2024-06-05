9c76f3c9923b7afb29e7e0c2c9ef77a74d6dda84
sagarssc97 <sagar.chauhan@fraazo.com>
2024-06-05 20:53:02 +0530

    gif

---
+++ 1/6.find_duplicates_from_file_io.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"io/ioutil"
+	"strings"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	for _, arg := range files {
+		data, err := ioutil.ReadFile(arg)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup2: %v\n",err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n"){
+			counts[line]++
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
